d27ee972da0cdeb6e1a62064bd9890a549ba3e48
EunjeongChoi414 <giving0414@naver.com>
2021-07-01 21:49:07 +0900

    test commit

---
+++ app/src/main/java/com/example/eatoo/src/main/create_group/CreateGroupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatoo.src.main.create_group
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_group)
+    }
+}
