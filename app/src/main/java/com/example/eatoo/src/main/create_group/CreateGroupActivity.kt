3cfea7b971d5258de000302e533df2f810cac53b
EunjeongChoi414 <giving0414@naver.com>
2021-07-02 14:26:40 +0900

    그룹 만들기 10%

--- app/src/main/java/com/example/eatoo/src/main/create_group/CreateGroupActivity.kt
+++ app/src/main/java/com/example/eatoo/src/main/create_group/CreateGroupActivity.kt
@@ -2,10 +2,14 @@ package com.example.eatoo.src.main.create_group
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eatoo.config.BaseActivity
+import com.example.eatoo.databinding.ActivityCreateGroupBinding
 
-class CreateGroupActivity : AppCompatActivity() {
+class CreateGroupActivity : BaseActivity<ActivityCreateGroupBinding>(ActivityCreateGroupBinding::inflate) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_group)
+
+
     }
 }
