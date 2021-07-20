c19baad6685b8cc18472c1fafe03db74b5fd6255
EunjeongChoi414 <giving0414@naver.com>
2021-07-21 00:43:40 +0900

    멤버 레이아웃 수정보완

--- app/src/main/java/com/example/eatoo/src/home/group/member/adapter/MemberRVAdapter.kt
+++ app/src/main/java/com/example/eatoo/src/home/group/member/adapter/MemberRVAdapter.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eatoo.R
 import com.example.eatoo.databinding.ItemMemberBinding
@@ -21,20 +22,30 @@ class MemberRVAdapter(
     inner class ViewHolder(val binding : ItemMemberBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bindItem(item : GroupMember, position : Int) {
-            binding.tvMemberNickname.text = item.nickName
-            val memberColor = if(item.color != 0) item.color -1 else 0
-            val memberChar = if(item.characters != 0) item.characters -1 else 0
-            binding.ivMember.setImageResource(EatooCharList[(memberColor*5) + memberChar])
-            binding.llMemberContainer.background = if(item.singleStatus == "OFF") ContextCompat.getDrawable(context, R.drawable.ic_ellipse_205)
-            else ContextCompat.getDrawable(context, R.drawable.ic_ellipse_208)
+            if(item.userIdx != -10){
+                binding.tvMemberNickname.text = item.nickName
+                val memberColor = if(item.color != 0) item.color -1 else 0
+                val memberChar = if(item.characters != 0) item.characters -1 else 0
+                binding.ivMember.setImageResource(EatooCharList[(memberColor*5) + memberChar])
+                if(item.singleStatus == "OFF") {
+                    binding.ivMemberContainer.setImageResource(R.drawable.background_member_orange)
+                } else {
+                    binding.ivMemberContainer.setImageResource(R.drawable.background_member_gray)
+                }
 
-            if(item.singleStatus == "ON"){
-                val grayScale = floatArrayOf(0.2989f, 0.5870f, 0.1140f,
-                    0F, 0f, 0.2989f, 0.5870f, 0.1140f, 0f, 0f, 0.2989f, 0.5870f, 0.1140f, 0f, 0f, 0.0000F, 0.0000F, 0.0000F, 1f, 0f)
-                val matrix = ColorMatrixColorFilter(grayScale)
-                binding.ivMember.colorFilter= matrix
+                if(item.singleStatus == "ON"){
+                    val grayScale = floatArrayOf(0.2989f, 0.5870f, 0.1140f,
+                        0F, 0f, 0.2989f, 0.5870f, 0.1140f, 0f, 0f, 0.2989f, 0.5870f, 0.1140f, 0f, 0f, 0.0000F, 0.0000F, 0.0000F, 1f, 0f)
+                    val matrix = ColorMatrixColorFilter(grayScale)
+                    binding.ivMember.colorFilter= matrix
+                }
 
+            }else {
+                binding.ivMemberContainer.setImageResource(R.drawable.background_member_gray)
+                binding.ivMember.isVisible = false
+                binding.ivAddMember.isVisible = true
             }
+
 
         }
     }
