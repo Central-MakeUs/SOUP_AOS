fa3c321867104853d88f343dccee8df1977db4fb
tmdrl980430 <tmdrl980430@naver.com>
2021-08-03 23:50:33 +0900

    초대장 찾기 부분 수정

--- app/src/main/java/com/makeus/eatoo/src/mypage/invite/InviteActivity.kt
+++ app/src/main/java/com/makeus/eatoo/src/mypage/invite/InviteActivity.kt
@@ -3,6 +3,8 @@ package com.makeus.eatoo.src.mypage.invite
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.makeus.eatoo.config.ApplicationClass
 import com.makeus.eatoo.config.BaseActivity
 import com.makeus.eatoo.databinding.ActivityInviteBinding
 import com.makeus.eatoo.src.mypage.invite.adapter.Invite_Group_Kind_RecyclerviewAdapter
@@ -16,6 +18,7 @@ class InviteActivity :  BaseActivity<ActivityInviteBinding>(ActivityInviteBindin
     lateinit var GroupName : String
     var GroupIdx : Int = 0
     var Click_status = 0
+    var Position = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,26 +36,40 @@ class InviteActivity :  BaseActivity<ActivityInviteBinding>(ActivityInviteBindin
                 binding.inviteBtn.visibility = View.GONE
             }
             else{
+                GroupIdx = response.result[0].groupIdx
+                GroupName = response.result[0].name
+                Click_status = 1
                 binding.myGroupRecyclerview.visibility = View.VISIBLE
                 binding.inviteBtn.visibility = View.VISIBLE
                 binding.mySuggestionNoneLayout.visibility = View.GONE
+
                 val GroupAdapter = Invite_Group_Kind_RecyclerviewAdapter(response.result)
                 binding.myGroupRecyclerview.adapter = GroupAdapter
                 binding.myGroupRecyclerview.layoutManager = LinearLayoutManager(this).also { it.orientation = LinearLayoutManager.HORIZONTAL }
-//            binding.indicator.attachTo(binding.myGroupRecyclerview, true)
-//            GroupAdapter.setItemClickListener(object :
-//                Invite_Group_Kind_RecyclerviewAdapter.ItemClickListener {
-//                override fun onClick(view: View, position: Int, groupIdx : Int , groupname : String) {
-//                    GroupIdx = groupIdx
-//                    GroupName = groupname
-//                }
-//            })
-                GroupAdapter.setItemClickListener(object :
-                    Invite_Group_Kind_RecyclerviewAdapter.ItemClickListener {
-                    override fun onClick(view: View, position: Int, groupIdx : Int ,groupname : String) {
-                        GroupIdx = groupIdx
-                        GroupName = groupname
-                        Click_status = 1
+//                GroupAdapter.setItemClickListener(object :
+//                    Invite_Group_Kind_RecyclerviewAdapter.ItemClickListener {
+//                    override fun onClick(view: View, position: Int, groupIdx : Int ,groupname : String) {
+//                        GroupIdx = groupIdx
+//                        GroupName = groupname
+//                        Click_status = 1
+//                    }
+//                })
+                binding.recyclerViewIndicator.setRecyclerView(binding.myGroupRecyclerview)
+                //binding.recyclerViewIndicator.forceUpdateItemCount()
+
+                binding.myGroupRecyclerview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrollStateChanged(
+                        recyclerView: RecyclerView,
+                        newState: Int
+                    ) {
+                        super.onScrollStateChanged(binding.myGroupRecyclerview, newState)
+                        when (newState) {
+                            RecyclerView.SCROLL_STATE_IDLE -> {
+                                Position =
+                                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findFirstCompletelyVisibleItemPosition()
+                                binding.recyclerViewIndicator.setCurrentPosition(Position)
+                            }
+                        }
                     }
                 })
             }
@@ -60,6 +77,11 @@ class InviteActivity :  BaseActivity<ActivityInviteBinding>(ActivityInviteBindin
 
             binding.inviteBtn.setOnClickListener {
                 if(Click_status == 1){
+                    GroupIdx = response.result[Position].groupIdx
+                    GroupName = response.result[Position].name
+                    ApplicationClass.sSharedPreferences.edit()
+                        .putString(ApplicationClass.GROUP_NAME,  GroupName).apply()
+                    Click_status = 1
                     InviteService(this).tryGetInviteCodeData(getUserIdx(),GroupIdx)
                 }
                 else{
