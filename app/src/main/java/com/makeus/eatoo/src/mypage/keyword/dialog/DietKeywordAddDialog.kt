f91eb37851b812240f04f0b493346c861ea0db5e
EunjeongChoi414 <giving0414@naver.com>
2021-08-06 18:25:03 +0900

    식성키워드 추가 유효성 검사 구현

--- app/src/main/java/com/makeus/eatoo/src/mypage/keyword/dialog/DietKeywordAddDialog.kt
+++ app/src/main/java/com/makeus/eatoo/src/mypage/keyword/dialog/DietKeywordAddDialog.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.RadioButton
 import android.widget.SeekBar
 import android.widget.Toast
@@ -30,17 +31,28 @@ class DietKeywordAddDialog(
         setSeekBar()
 
         binding.btnDialogConfirm.setOnClickListener {
-            if(seekbarLevel == 0) {
-                Toast.makeText(context, "강조 레벨을 입력해주세요.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            val radioBtnPrefer
-                    = binding.radiogroupDietKeyword.findViewById<RadioButton>(binding.radiogroupDietKeyword.checkedRadioButtonId).tag.toString()
-                .toInt()
-            val isPrefer = if(radioBtnPrefer==1) "Y" else "N"
-            listener.onDietKeywordAddClicked(DietKeywordInfo(binding.etDietKeywordInput.text.toString(), isPrefer, seekbarLevel!!))
-            dismiss()
+            checkValidation()
         }
+    }
+
+    private fun checkValidation() {
+        val radioBtnPrefer
+                = binding.radiogroupDietKeyword.findViewById<RadioButton>(binding.radiogroupDietKeyword.checkedRadioButtonId).tag.toString()
+            .toInt()
+
+        if(binding.etDietKeywordInput.text.isEmpty()) {
+            Toast.makeText(context, "키워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(seekbarLevel == 0) {
+            Toast.makeText(context, "강조 레벨을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val isPrefer = if(radioBtnPrefer==1) "Y" else "N"
+        listener.onDietKeywordAddClicked(DietKeywordInfo(binding.etDietKeywordInput.text.toString(), isPrefer, seekbarLevel!!))
+        dismiss()
     }
 
     private fun setSeekBar() {
